f0eac4bfc5d3016feb6926bce19222e661ddbd5e
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-01 13:46:05 +0530

    Initialize React app with Vite and component structure
    Set up a new React project using Vite, including initial configuration files, ESLint setup, and a basic component structure with Navbar, Card, and Footer components. Added related CSS files and assets, as well as project metadata and documentation.

---
+++ ReactJS/Day_2 Component-Props-jsx/First-app/src/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      Copyright &copy; www.nikhilagr.com | All rights reserved
+    </div>
+  );
+};
+
+export default Footer;
